716cbb40b38511321145a69213cc2216fbb138d3
tmkhael <tmkhael@murex.com>
2016-02-02 10:56:02 +0100

    Initial commit

---
+++ src/main/java/MyFile.java
@@ -0,0 +1,16 @@
+//tag::all[]
+public interface MyCoolService {
+
+  void notNeeded1();
+
+  // tag::include_this[]
+  void operation1();
+
+  void operation2();
+
+  void operation3();
+  // end::include_this[]
+
+  void notNeeded2();
+}
+//end::all[]
